5d12d0942db2ec6a32a8794bb4d1fe0e584fa5eb
lenzees <tcely2003@gmail.com>
2023-11-23 11:23:39 +0100

    salles

---
+++ salles.php
@@ -0,0 +1,27 @@
+<?php
+class salles {
+    private $id;
+    private $nom;
+    private $type;
+    private $description;
+
+    public function __construct($id, $nom, $type ,$description) {
+        $this->id = $id;
+        $this->nom = $nom;
+        $this->type = $type;
+        $this->description = $description;
+    }
+
+    public function getId(){
+        return $this->id;
+    }
+    public function getNom(){
+        return $this->nom;
+    }
+    public function getType(){
+        return $this->type;
+    }
+    public function getDescription(){
+        return $this->description;
+    }
+    }
